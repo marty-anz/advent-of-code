25cce7584bba06e814e38ce3648d5a5fc8c4a8cf
Tie Ma <tie.ma@anz.com>
2024-12-05 11:54:21 +1100

    shortened day 3

--- 2024/03/day.rb
+++ 2024/03/day.rb
@@ -11,9 +11,7 @@ aoc = Aoc.new(__dir__) do |data|
 end
 
 def part1(input)
-  input.join.scan(/mul\(\d+,\d+\)/).sum do |m|
-    m.scan(/\d+/).map(&:to_i).inject(:*)
-  end
+  input.join.scan(/mul\(\d+,\d+\)/).sum { |m| m.scan(/\d+/).map(&:to_i).inject(:*) }
 end
 
 puts part1(aoc.test_data)
@@ -21,9 +19,10 @@ puts part1(aoc.test_data)
 puts part1(aoc.data)
 
 def part2(input)
-  input.join.scan(/mul\(\d+,\d+\)|do\(\)|don\'t\(\)/).each_with_object([true, []]) do |m, state|
-    m.start_with?('do') ? state[0] = m == "do()" : (state[1] << m if state[0])
-  end[1].sum {|m| m.scan(/\d+/).map(&:to_i).inject(:*) }
+  input.join.
+    scan(/mul\(\d+,\d+\)|do\(\)|don\'t\(\)/).
+    each_with_object([true, []]) { |m, state| m.start_with?('do') ? state[0] = m == 'do()' : (state[1] << m if state[0]) }[1].
+    sum {|m| m.scan(/\d+/).map(&:to_i).inject(:*) }
 end
 
 puts part2(["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"])
